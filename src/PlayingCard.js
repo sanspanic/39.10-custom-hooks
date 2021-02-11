9562ee83c7d0026d5ef7d82698c12c3cff4f1b70
Sandra Spanikova <spanikovasandra@gmail.com>
2021-02-11 13:06:59 +0100

    write custom hook: toggle flip card

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
-import "./PlayingCard.css"
+import "./PlayingCard.css";
+import useFlip from "./hooks/useFlip";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
+  /*   const [isFacingUp, setIsFacingUp] = useState(true);
   const flipCard = () => {
     setIsFacingUp(isUp => !isUp);
-  };
+  }; */
+
+  const [isFacingUp, toggleIsFacingUp] = useFlip();
+
   return (
     <img
       src={isFacingUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={toggleIsFacingUp}
       className="PlayingCard Card"
     />
   );
---
+++ src/hooks/useFlip.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const useFlip = (initialState = true) => {
+  const [state, setState] = useState(initialState);
+
+  const toggleState = () => {
+    setState((state) => !state);
+  };
+
+  return [state, toggleState];
+};
+
+export default useFlip;
